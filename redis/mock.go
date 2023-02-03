1dbcad9c1dd075a2c511bcd75fb1436f8e0b51d8
Andrey Kalinin <kalinin.andrey1@mail.ru>
2023-02-03 15:11:18 +0300

    fix

--- redis/mock.go
+++ redis/mock.go
@@ -1,29 +1,31 @@
 package redis
 
 import (
-	"github.com/alicebob/miniredis"
+	//"github.com/alicebob/miniredis"
 	"github.com/elliotchance/redismock/v8"
-	gocache "github.com/go-redis/cache/v8"
-	"github.com/go-redis/redis/v8"
+	//gocache "github.com/go-redis/cache/v8"
+	//"github.com/go-redis/redis/v8"
+	"github.com/pkg/errors"
 )
 
 // New creates a new mock client.
 func NewMock() (*DB, *redismock.ClientMock, error) {
-	mr, err := miniredis.Run()
-	if err != nil {
-		return nil, nil, err
-	}
-
-	client := redis.NewClient(&redis.Options{
-		Addr: mr.Addr(),
-	})
-
-	mock := redismock.NewNiceMock(client)
-	dbobj := &DB{
-		Exec: mock,
-		cache: gocache.New(&gocache.Options{
-			Redis: mock,
-		}),
-	}
-	return dbobj, mock, nil
+	//mr, err := miniredis.Run()
+	//if err != nil {
+	//	return nil, nil, err
+	//}
+	//
+	//client := redis.NewClient(&redis.Options{
+	//	Addr: mr.Addr(),
+	//})
+	//
+	//mock := redismock.NewNiceMock(client)
+	//dbobj := &DB{
+	//	Exec: mock,
+	//	cache: gocache.New(&gocache.Options{
+	//		Redis: mock,
+	//	}),
+	//}
+	//return dbobj, mock, nil
+	return nil, nil, errors.New("This code under construction.")
 }
